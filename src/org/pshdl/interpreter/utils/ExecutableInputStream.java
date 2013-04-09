b043f81417644d882ec8dc771592f3d4b107c1cc
Karsten Becker <KB@Karsten-Becker.de>
2013-04-09 14:41:07 +0200

    Refactored to decorated Streams TLV now uses VLI to indicate length

---
+++ src/org/pshdl/interpreter/utils/ExecutableInputStream.java
@@ -0,0 +1,242 @@
+package org.pshdl.interpreter.utils;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+import org.pshdl.interpreter.*;
+import org.pshdl.interpreter.utils.IOUtil.*;
+
+public class ExecutableInputStream extends DataInputStream {
+
+	protected ExecutableInputStream(InputStream in) {
+		super(in);
+	}
+
+	private class TLV {
+		public final Enum<?> type;
+		public final byte[] value;
+
+		private TLV(Enum<?> type, byte[] value) {
+			super();
+			this.type = type;
+			this.value = value;
+		}
+
+	}
+
+	public TLV readTLV(Enum<?> e[]) throws IOException {
+		int len = 0;
+		int type = -1;
+		type = read();
+		if (type == -1)
+			return null;
+		if ((type & 0x80) != 0) {
+			type &= 0x7F;
+		}
+		len = readVarInt();
+		// System.out.println("IOUtil.TLV.read()Reading type:" + e[type] +
+		// " with len:" + len);
+		byte data[] = new byte[len];
+		readFully(data);
+		Enum<?> ne = e[type];
+		return new TLV(ne, data);
+	}
+
+	public ExecutableModel readExecutableModel(boolean verbose) throws IOException {
+		TLV tlv = null;
+		byte[] header = new byte[4];
+		readFully(header);
+		if (!"PSEX".equals(new String(header)))
+			throw new IllegalArgumentException("Not a PS Executable: Missing or wrong header!");
+		int[] widths = new int[0];
+		String[] internals = new String[0];
+		List<Frame> frameList = new LinkedList<Frame>();
+		while ((tlv = readTLV(ModelTypes.values())) != null) {
+			ModelTypes type = (ModelTypes) tlv.type;
+			ExecutableInputStream ex = new ExecutableInputStream(new ByteArrayInputStream(tlv.value));
+			switch (type) {
+			case date:
+				if (verbose) {
+					long readDate = ex.readLong();
+					System.out.printf("Created on: %1$F %1$T\n" + new Date(readDate));
+				}
+				break;
+			case frame:
+				frameList.add(ex.readFrame(verbose));
+				break;
+			case internals:
+				internals = ex.readStringArray();
+				break;
+			case maxDataWidth:
+				if (verbose) {
+					int maxDataWidth = ex.readVarInt();
+					System.out.println("Max data width:" + maxDataWidth);
+				}
+				break;
+			case maxStackDepth:
+				if (verbose) {
+					int maxStackDepth = ex.readVarInt();
+					System.out.println("Max Stack depth:" + maxStackDepth);
+				}
+				break;
+			case registers:
+				if (verbose) {
+					int[] asIntArray = ex.readIntArray();
+					StringBuilder sb = new StringBuilder();
+					for (int i : asIntArray) {
+						sb.append(' ').append(internals[i]);
+					}
+					System.out.println("The following internals are registers:" + sb);
+				}
+				break;
+			case src:
+				if (verbose) {
+					String src = new String(tlv.value, "UTF-8");
+					System.out.println("Generated from resource:" + src);
+				}
+				break;
+			case version:
+				if (verbose) {
+					byte[] version = new byte[3];
+					ex.readFully(version);
+					System.out.printf("Compiled with version: %d.%d.%d\n", version[0], version[1], version[2]);
+				}
+				break;
+			case widths:
+				widths = ex.readIntArray();
+				break;
+			default:
+				ex.close();
+				throw new IllegalArgumentException("The type:" + type + " is not handled");
+			}
+			ex.close();
+		}
+		Frame[] frames = frameList.toArray(new Frame[frameList.size()]);
+		Map<String, Integer> widthMap = new HashMap<String, Integer>();
+		for (int i = 0; i < internals.length; i++) {
+			String s = internals[i];
+			widthMap.put(s, widths[i]);
+		}
+		return new ExecutableModel(frames, internals, widthMap);
+	}
+
+	public Frame readFrame(boolean verbose) throws IOException {
+		TLV tlv = null;
+		BigInteger consts[] = new BigInteger[0];
+		int edgeNegDep = -1, edgePosDep = -1;
+		int[] predNegDep = null;
+		int[] predPosDep = null;
+		int executionDep = -1;
+		int maxDataWidth = -1, maxStackDepth = -1;
+		int outputID = -1, uniqueID = -1;
+		byte[] instructions = new byte[0];
+		int[] intDeps = new int[0];
+		while ((tlv = readTLV(FrameTypes.values())) != null) {
+			ExecutableInputStream ex = new ExecutableInputStream(new ByteArrayInputStream(tlv.value));
+			FrameTypes type = (FrameTypes) tlv.type;
+			switch (type) {
+			case constants:
+				String[] strings = ex.readStringArray();
+				consts = new BigInteger[strings.length];
+				for (int i = 0; i < strings.length; i++) {
+					String string = strings[i];
+					consts[i] = new BigInteger(string, 16);
+				}
+				break;
+			case edgeNegDep:
+				edgeNegDep = ex.readVarInt();
+				break;
+			case edgePosDep:
+				edgePosDep = ex.readVarInt();
+				break;
+			case executionDep:
+				executionDep = ex.readVarInt();
+				break;
+			case instructions:
+				instructions = tlv.value;
+				break;
+			case internalDep:
+				intDeps = ex.readIntArray();
+				break;
+			case maxDataWidth:
+				maxDataWidth = ex.readVarInt();
+				break;
+			case maxStackDepth:
+				maxStackDepth = ex.readVarInt();
+				break;
+			case outputID:
+				outputID = ex.readVarInt();
+				break;
+			case predNegDep:
+				predNegDep = ex.readIntArray();
+				break;
+			case predPosDep:
+				predPosDep = ex.readIntArray();
+				break;
+			case uniqueID:
+				uniqueID = ex.readVarInt();
+				break;
+			default:
+				ex.close();
+				throw new IllegalArgumentException("The type:" + type + " is not handled");
+			}
+			ex.close();
+		}
+		Frame frame = new Frame(instructions, intDeps, predPosDep, predNegDep, edgePosDep, edgeNegDep, outputID, maxDataWidth, maxStackDepth, consts, uniqueID);
+		frame.executionDep = executionDep;
+		return frame;
+	}
+
+	public String[] readStringArray() throws IOException {
+		int amount = readVarInt();
+		String[] res = new String[amount];
+		for (int i = 0; i < amount; i++) {
+			res[i] = readString();
+		}
+		return res;
+	}
+
+	public String readString() throws IOException {
+		int len = readVarInt();
+		byte[] buf = new byte[len];
+		readFully(buf);
+		return new String(buf, "UTF-8");
+	}
+
+	public int[] readIntArray() throws IOException {
+		int amount = readVarInt();
+		int res[] = new int[amount];
+		for (int i = 0; i < amount; i++) {
+			res[i] = readVarInt();
+		}
+		return res;
+	}
+
+	public int readVarInt() throws IOException {
+		int tmp = 0;
+		if (((tmp = read()) & 0x80) == 0)
+			return tmp;
+		int result = tmp & 0x7f;
+		if (((tmp = read()) & 0x80) == 0) {
+			result |= tmp << 7;
+		} else {
+			result |= (tmp & 0x7f) << 7;
+			if (((tmp = read()) & 0x80) == 0) {
+				result |= tmp << 14;
+			} else {
+				result |= (tmp & 0x7f) << 14;
+				if (((tmp = read()) & 0x80) == 0) {
+					result |= tmp << 21;
+				} else {
+					result |= (tmp & 0x7f) << 21;
+					result |= (tmp = read()) << 28;
+					if (tmp < 0)
+						throw new IllegalArgumentException("Too many bits");
+				}
+			}
+		}
+		return result;
+	}
+
+}
---
+++ src/org/pshdl/interpreter/utils/ExecutableOutputStream.java
@@ -0,0 +1,150 @@
+package org.pshdl.interpreter.utils;
+
+import java.io.*;
+
+import org.pshdl.interpreter.*;
+import org.pshdl.interpreter.utils.IOUtil.*;
+
+public class ExecutableOutputStream extends DataOutputStream {
+
+	public ExecutableOutputStream(OutputStream out) {
+		super(out);
+	}
+
+	public void writeExecutableModel(String source, long date, ExecutableModel model) throws IOException {
+		write("PSEX".getBytes());
+		writeByteArray(ModelTypes.version, new byte[] { 0, 2, 0 });
+		writeString(ModelTypes.src, source);
+		if (date != -1) {
+			writeSingleLong(ModelTypes.date, date);
+		}
+		writeInt(ModelTypes.maxDataWidth, model.maxDataWidth);
+		writeInt(ModelTypes.maxStackDepth, model.maxStackDepth);
+		writeStringArray(ModelTypes.internals, model.internals);
+		int[] widths = new int[model.internals.length];
+		for (int i = 0; i < widths.length; i++) {
+			// Transform map into sequence of width with same ordering as
+			// internals
+			widths[i] = model.getWidth(model.internals[i]);
+		}
+		writeIntArray(ModelTypes.widths, widths);
+		writeIntArray(ModelTypes.registers, model.registerOutputs);
+		for (Frame f : model.frames) {
+			writeFrame(f);
+		}
+	}
+
+	public void writeFrame(Frame f) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ExecutableOutputStream obj = new ExecutableOutputStream(baos);
+		obj.writeInt(FrameTypes.uniqueID, f.uniqueID);
+		obj.writeInt(FrameTypes.outputID, f.outputId);
+		obj.writeIntArray(FrameTypes.internalDep, f.internalDependencies);
+		// Only write if they are set
+		if (f.edgeNegDepRes != -1) {
+			obj.writeInt(FrameTypes.edgeNegDep, f.edgeNegDepRes);
+		}
+		if (f.edgePosDepRes != -1) {
+			obj.writeInt(FrameTypes.edgePosDep, f.edgePosDepRes);
+		}
+		if ((f.predNegDepRes != null) && (f.predNegDepRes.length > 0)) {
+			obj.writeIntArray(FrameTypes.predNegDep, f.predNegDepRes);
+		}
+		if ((f.predPosDepRes != null) && (f.predPosDepRes.length > 0)) {
+			obj.writeIntArray(FrameTypes.predPosDep, f.predPosDepRes);
+		}
+		if (f.executionDep != -1) {
+			obj.writeInt(FrameTypes.executionDep, f.executionDep);
+		}
+		String[] consts = new String[f.constants.length];
+		for (int i = 0; i < consts.length; i++) {
+			consts[i] = f.constants[i].toString(16); // Represent as hex String
+		}
+		obj.writeStringArray(FrameTypes.constants, consts);
+		obj.writeByteArray(FrameTypes.instructions, f.instructions);
+		obj.writeInt(FrameTypes.maxDataWidth, f.maxDataWidth);
+		obj.writeInt(FrameTypes.maxStackDepth, f.maxStackDepth);
+
+		writeByteArray(ModelTypes.frame, baos.toByteArray());
+		obj.close();
+	}
+
+	/**
+	 * Simple TLV for single integers. No preceeding items count
+	 */
+	public void writeInt(Enum<?> e, int data) throws IOException {
+		writeByteArray(e, getVarInt(data));
+	}
+
+	public static byte[] getVarInt(int val) throws IOException {
+		int num = val;
+		int t = 0;
+		ByteArrayOutputStream os = new ByteArrayOutputStream(5);
+		while (num > 127) {
+			t = 0x80 | (num & 0x7F);
+			os.write(t);
+			num >>= 7;
+		}
+		t = num & 0x7F;
+		os.write(t);
+		return os.toByteArray();
+	}
+
+	public static void main(String[] args) throws IOException {
+		byte[] varInt = getVarInt(123456);
+		for (byte b : varInt) {
+			System.out.printf("%02X", b & 0xFF);
+		}
+	}
+
+	public void writeIntArray(Enum<?> e, int... data) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		for (int i : data) {
+			baos.write(getVarInt(i));
+		}
+		byte[] lenHeader = getVarInt(data.length);
+		byte[] varIntArray = baos.toByteArray();
+		writeHeader(e, varIntArray.length + lenHeader.length);
+		write(lenHeader); // Write the number of elements
+		write(varIntArray); // Write all VarInts
+	}
+
+	public void writeString(Enum<?> e, String data) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] bytes = data.getBytes("UTF-8");
+		baos.write(bytes);
+		writeByteArray(e, data.getBytes("UTF-8"));
+	}
+
+	public void writeStringArray(Enum<?> e, String... data) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.write(getVarInt(data.length));
+		for (String string : data) {
+			byte[] bytes = string.getBytes("UTF-8");
+			baos.write(getVarInt(bytes.length));
+			baos.write(bytes);
+		}
+		writeByteArray(e, baos.toByteArray());
+	}
+
+	public void writeSingleLong(Enum<?> e, long data) throws IOException {
+		writeHeader(e, 8);
+		writeLong(data);
+	}
+
+	public void writeByteArray(Enum<?> e, byte[] bytes) throws IOException {
+		int len = bytes.length;
+		writeHeader(e, len);
+		write(bytes, 0, len); // Write data
+	}
+
+	private void writeHeader(Enum<?> e, int len) throws IOException {
+		if (e instanceof FrameTypes) {
+			writeByte(e.ordinal() | 0x80); // Write 1 byte type field
+		} else {
+			writeByte(e.ordinal()); // Write 1 byte type field
+		}
+		write(getVarInt(len)); // Write 2 byte length field
+	}
+
+}
